a35150216e4e9a80122046f801fc5ba08bdebbe5
Ayush <ayushchawda123@gmail.com>
2025-06-11 17:30:05 +0530

    Practice Routing

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Application__using_Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//Converstion based Routing
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}"
+);
+
+//app.UseRouting();
+
+//app.UseEndpoints(endpoint  =>
+//{
+//    endpoint.MapGet("/", async (context) =>
+//    {
+//        await context.Response.WriteAsync("<h1>This is home page Get Api</h1>");
+//    });
+//    endpoint.MapGet("/about", async (context) =>
+//    {
+//        await context.Response.WriteAsync("<h1>This is about page Get Api</h1>");
+//    });
+
+//    endpoint.MapGet("/contact", async (context) =>
+//    {
+//        await context.Response.WriteAsync("<h1>This is Contact page Get Api</h1>");
+//    });
+
+//});
 
 app.Run();
