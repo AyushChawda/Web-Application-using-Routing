0bc8ab087c3c4775cde156b0085f375117c18f5d
Ayush <ayushchawda123@gmail.com>
2025-06-14 22:43:26 +0530

    views updates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,10 +6,30 @@ namespace Web_Application__using_Routing.Controllers
     {
         public IActionResult Index()
         {
+
+            // Passing the data with the help of ViewData
+            string[] arr=["Shivam", "Pawan", "Abhishek","Shivaay"];
+            ViewData["data"] = arr;
+
+            List<string> userName = new List<string> { "Shivam","Gopal","Vaibhav","Suresh","Govind" };
+            ViewData["UserList"] = userName;
+
+            // Passing data using viewBag
+
+            ViewBag.name = "Ayush";
+            ViewBag.list = arr;
+
+            // Passing data using tempData
+
+            TempData["list"] = new List<string> { "Raghav", "Shruti", "Vishal", "Shivangi" };
+            TempData["name"] = "ayush";
+            TempData.Keep();
+
             return View();
         }
         public IActionResult About()
         {
+            TempData.Keep();
             return View();
         }
     }
